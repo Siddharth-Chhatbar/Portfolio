ae4e44a0496441777fa6656174dd9e5f81448736
Siddharth-Chhatbar <chhatbar@ualberta.ca>
2024-10-29 18:47:50 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import ProgrammingPage from "./pages/ProgrammingPage";
-import PhotographyPage from "./pages/PhotographyPage";
+// import PhotographyPage from "./pages/PhotographyPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/programming" element={<ProgrammingPage />} />
-        <Route path="/photography" element={<PhotographyPage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<ProgrammingPage />} />
+        {/* <Route path="/photography" element={<PhotographyPage />} /> */}
       </Routes>
     </BrowserRouter >
   );
