c05654ed10f66ab321ce5b8b60bedb47f9e504a3
Siddharth-Chhatbar <chhatbar@ualberta.ca>
2024-10-29 18:55:25 -0600

    email

--- src/components/ContactCommand.tsx
+++ src/components/ContactCommand.tsx
@@ -4,8 +4,8 @@ const ContactCommand = () => {
       <div className="flex flex-row gap-4">
         <div>portfolio@Siddharth-Chhatbar: ~/programming$</div>
         Contact Info:
-        <a href="mailto:example@example.com?subject=Contacting%20via%20Your%20Portfolio&body=Hello,%0D%0A%0D%0AI%20found%20your%20portfolio%20and%20would%20like%20to%20discuss%20a%20potential%20project%20collaboration.%0D%0A%0D%0ALet%20me%20know%20when%20you%20are%20available.%0D%0A%0D%0AThank%20you!">
-          Email Me
+        <a className=" underline" href="mailto:example@example.com?subject=Contacting%20via%20Your%20Portfolio&body=Hello,%0D%0A%0D%0AI%20found%20your%20portfolio%20and%20would%20like%20to%20discuss%20a%20potential%20project%20collaboration.%0D%0A%0D%0ALet%20me%20know%20when%20you%20are%20available.%0D%0A%0D%0AThank%20you!">
+          siddharth3901@gmail.com (Email Me)
         </a>
       </div>
     </div>
