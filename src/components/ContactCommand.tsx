cf55457b754ca3310453fa3dcf88d30b4dc1510f
Siddharth-Chhatbar <112894386+Siddharth-Chhatbar@users.noreply.github.com>
2025-04-15 11:00:35 +0900

    Update ContactCommand.tsx

--- src/components/ContactCommand.tsx
+++ src/components/ContactCommand.tsx
@@ -4,7 +4,7 @@ const ContactCommand = () => {
       <div className="flex flex-row gap-4">
         <div>portfolio@Siddharth-Chhatbar: ~/programming$</div>
         Contact Info:
-        <a className=" underline" href="mailto:example@example.com?subject=Contacting%20via%20Your%20Portfolio&body=Hello,%0D%0A%0D%0AI%20found%20your%20portfolio%20and%20would%20like%20to%20discuss%20a%20potential%20project%20collaboration.%0D%0A%0D%0ALet%20me%20know%20when%20you%20are%20available.%0D%0A%0D%0AThank%20you!">
+        <a className=" underline" href="mailto:siddharth3901@gmail.com?subject=Contacting%20via%20Your%20Portfolio&body=Hello,%0D%0A%0D%0AI%20found%20your%20portfolio%20and%20would%20like%20to%20discuss%20a%20potential%20project%20collaboration.%0D%0A%0D%0ALet%20me%20know%20when%20you%20are%20available.%0D%0A%0D%0AThank%20you!">
           siddharth3901@gmail.com (Email Me)
         </a>
       </div>
